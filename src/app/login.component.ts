b72149d7c396b4b2c9aa622077c1eb0115abc199
Dorian Delorme <dorian.delorme@hetic.net>
2017-02-20 23:11:04 +0100

    Create login component

---
+++ src/app/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  template: `
+    <div class="login">
+      <div class="login-title">
+        <h1 class="login-title__text">Colibrique v2</h1>
+      </div>
+      <form class="login__form">
+        <div class="login__form-instructions">Veuillez vous connecter</div>
+        <input class="login__form-input" placeholder="Identifiant">
+        <input class="login__form-input" placeholder="Mot de passe">
+        <div class="login__form-remember">
+          <input type="radio">
+          <p class="login__form-remember-text">Se souvenir de moi</p>
+        </div>
+        <input type="submit" class="login__form-btn btn">
+      </form>
+    </div>
+    `
+})
+
+export class LoginComponent {
+}
