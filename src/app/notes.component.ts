f0d3c109b06e41dddb4e80d95569ce6a15d7fa3a
Thomas Sanlis <Thomas.sanlis@gmail.com>
2017-02-23 20:08:38 +0100

    Update Notes Component

--- src/app/notes.component.ts
+++ src/app/notes.component.ts
@@ -4,7 +4,34 @@ import { Component } from '@angular/core';
   selector: 'notes',
   template: `
     <navigation></navigation>
-    `
+    <div class="wrapper">
+    <div class="title">Accéder aux notes</div>
+    <select>
+      <option value="1">Trimestre 1</option>
+      <option value="2">Trimestre 2</option>
+      <option value="3">Trimestre 3</option>
+    </select>
+    <button>Télécharger les notes</button>
+    <div class="subtitle">Notes du trimestre en cours</div>
+    <div class="notes-curses">
+      <div class="notes-curse">
+        <div class="notes-curse__title">PHP & SYMFONY</div>
+        <div class="class-curse__first">- Controle continu : 9/20</div>
+        <div class="class-curse__second">- Partiel : 10/20</div>
+      </div>
+      <div class="notes-curse">
+       <div class="notes-curse__title">PHP & SYMFONY</div>
+        <div class="class-curse__first">- Controle continu : 9/20</div>
+        <div class="class-curse__second">- Partiel : 10/20</div>
+      </div>
+      <div class="notes-curse">
+       <div class="notes-curse__title">PHP & SYMFONY</div>
+        <div class="class-curse__first">- Controle continu : 9/20</div>
+        <div class="class-curse__second">- Partiel : 10/20</div>
+      </div>
+    </div>
+    </div>
+  `
 })
 
 export class NotesComponent {
